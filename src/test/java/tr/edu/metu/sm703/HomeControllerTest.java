7fea911dae4962e400148e97ece63050708ca8ad
Mustafa <mustafa_gvn@hotmail.com>
2023-06-14 23:17:27 +0300

    test fixed

--- src/test/java/tr/edu/metu/sm703/HomeControllerTest.java
+++ src/test/java/tr/edu/metu/sm703/HomeControllerTest.java
@@ -38,6 +38,6 @@ public class HomeControllerTest {
         request.setPath("/");
         AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
         assertEquals(200, response.getStatusCode());
-        assertEquals("{\"Sum\":8}",  response.getBody());
+        assertEquals("{\"Sum of Integers\":8}",  response.getBody());
     }
 }
