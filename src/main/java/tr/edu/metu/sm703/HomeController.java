36d98e5e99d67c2c9e12e97b8ea4af56cd5a1139
Mustafa <mustafa_gvn@hotmail.com>
2023-06-16 20:20:45 +0300

    triggering codeql

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -1,6 +1,10 @@
 package tr.edu.metu.sm703;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -8,10 +12,20 @@ import java.util.Map;
 public class HomeController {
 
     @Get
-    public Map<String, Object> index() {
+    public Map<String, Object> index() throws NoSuchPaddingException, NoSuchAlgorithmException {
         int a = 5;
         int b = 3;
 
+        String secretKey = getSecretKey();
+        Cipher c = Cipher.getInstance("DES");
+
         return Collections.singletonMap("Sum", a+b);
+    }
+
+    public String getSecretKey()
+    {
+        String secretKey = "1234";
+
+        return secretKey;
     }
 }
